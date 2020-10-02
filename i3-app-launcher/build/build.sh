3664f22923f241bf2ad5d7844492c0f200982979
Daniel Treffenstädt <daniel@treffenstaedt.de>
2020-10-02 07:58:30 +0200

    Changed build system to cmake

--- i3-app-launcher/build/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if g++ -c `find ../src -type f -iname "*.cpp"` -v ; then
-if g++ `find . -type f -iname "*.o"` -o i3-app-launcher -lsfml-graphics -lsfml-window -lsfml-system -v -O3; then
-echo -e "\ndone building!"
-fi
-fi
